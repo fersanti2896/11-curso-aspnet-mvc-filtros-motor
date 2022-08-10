564e5375e1cc9bd0814d92f9c505b8ca4fdeb070
Fernando Nicolás <fersanti2896@gmail.com>
2022-08-10 02:05:48 -0500

    Subiendo proyecto desde S9 y S10

---
+++ Controllers/ConsultaInformacionController.cs
@@ -0,0 +1,33 @@
+﻿using Dominio.IServicios;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Transporte;
+
+namespace Pedidos.Controllers {
+    public class ConsultaInformacionController : Controller {
+        private readonly IConsultaPedidoService _IConsultaPedidoService;
+
+        public ConsultaInformacionController(IConsultaPedidoService iConsultaPedidoService) {
+            _IConsultaPedidoService = iConsultaPedidoService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> ConsultaPedidosGeneral(ModeloConsultaDTO modelo) {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            var lista = await _IConsultaPedidoService.ConsultaPedidosGeneral(modelo);
+
+            stopwatch.Stop();
+            TimeSpan ts = stopwatch.Elapsed;
+
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+
+            return PartialView(lista);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+using Dominio.IRepository;
+using Dominio.IServicios;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Persistencia.Context;
+using Persistencia.Repository;
+using Servicios.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var connectionString = builder.Configuration.GetConnectionString("Conexion");
+
+builder.Services.AddDbContext<ApplicationDbContext>(
+                    opciones => { opciones.UseSqlServer( connectionString ); }
+                 );
+
+builder.Services.AddScoped<IConsultaGraficaService, ConsultaGraficaService>();
+builder.Services.AddScoped<IConsultaGraficaRepository, ConsultaGraficaRepository>();
+builder.Services.AddScoped<IConsultaPedidoService, ConsultaPedidoService>();
+builder.Services.AddScoped<IConsultaPedidoRepository, ConsultaPedidoRepository>();
+
+var app = builder.Build();
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}");
+
+app.Run();
